212e95fc81d2c7273d00d3f198bc89b14c812ebd
Avazbekov Asadbek <117262063+AvazbekovAsadbek2001@users.noreply.github.com>
2025-10-20 12:49:19 +0500

    and employee db

---
+++ src/database/migrations/2025_10_20_062040_create_employees_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->data('bithday_data');
+            $table->email('email')->nullable();
+            $table->integer('phone')->nullable();
+            $table->string('schedule_days')->default('Dushanba - Juma');
+            $table->string('schedule_time')->default('09:00 - 17:00');
+            
+            $table->integer('nation_id')->unsigned();
+            $table->foreign('nation_id')->references('id')->on('nations');
+            
+            $table->integer('position_id')->unsigned();
+            $table->foreign('position_id')->references('id')->on('positions');
+
+            $table->timestamps();   
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
